f6be8e4e683fea2c83cac20ca440269c60e63cbe
yakoushi <missi.iberrakene@gmail.com>
2018-03-13 04:01:05 +0100

    modif contact && index

---
+++ production/js/listeSettings.js
@@ -0,0 +1,23 @@
+
+function addMembres() {
+	
+	var membre
+	var label
+	
+	
+	membre=document.getElementById("membres").value
+	
+    $("#divLabelMembres").append("<div class='form control col-md-2'></div><label for=\"name\">Test</label></div>");
+	
+	document.getElementById("membres").value=""
+	
+	
+	/*indicecondition++;
+    var divselect = document.getElementById("condition");
+    var newselect = createDivCondition(indicecondition);
+    insertAfter(divselect, newselect)
+    $("#c_"+indicecondition).selectpicker('refresh')
+    tableauIdCondition.push("#c_"+indicecondition);*/
+	
+	
+}
